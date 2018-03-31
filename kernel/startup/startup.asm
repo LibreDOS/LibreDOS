d2288ed0d9b2cae4fb53c2804426513c5d3e51ba
mintsuki <xenos1@protonmail.com>
2018-03-31 14:26:00 +0200

    Added real mode trampoline

--- kernel/startup/startup.asm
+++ kernel/startup/startup.asm
@@ -7,8 +7,87 @@ section .text
 
 bits 32
 _main:
-    mov byte [0xb8000], 'a'
+    mov esi, trampoline - 0x10 + 0x100000
+    mov edi, 0x1000
+    mov ecx, trampoline.end - trampoline
+    rep movsb
+
+    ; exit protected mode
+    lidt [real_mode_idt - 0x10 + 0x100000]
+    lgdt [real_mode_gdt - 0x10 + 0x100000]
+
+    mov ax, 0x0010
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    jmp 0x0008:0x1000
+
+bits 16
+  .realmode:
+    mov ax, 0xb800
+    mov es, ax
+    mov byte [es:0x0000], 'a'
     jmp $
+
+bits 16
+trampoline:
+    mov eax, cr0
+    and al, 0xfe
+    mov cr0, eax
+
+    xor ax, ax
+    not ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov sp, 0xfff0
+
+    jmp 0xffff:_main.realmode
+
+  .end:
+
+align 16
+real_mode_idt:
+    dw 0x3ff
+    dd 0
+
+align 16
+real_mode_gdt:
+    dw .end - .start - 1
+    dd .start - 0x10 + 0x100000
+
+align 16
+    .start:
+
+    ; Null descriptor (required)
+    dw 0x0000           ; Limit
+    dw 0x0000           ; Base (low 16 bits)
+    db 0x00             ; Base (mid 8 bits)
+    db 00000000b        ; Access
+    db 00000000b        ; Granularity
+    db 0x00             ; Base (high 8 bits)
+
+    ; Real mode code
+    dw 0xFFFF           ; Limit
+    dw 0x0000           ; Base (low 16 bits)
+    db 0x00             ; Base (mid 8 bits)
+    db 10011010b        ; Access
+    db 00000000b        ; Granularity
+    db 0x00             ; Base (high 8 bits)
+
+    ; Real mode data
+    dw 0xFFFF           ; Limit
+    dw 0x0000           ; Base (low 16 bits)
+    db 0x00             ; Base (mid 8 bits)
+    db 10010010b        ; Access
+    db 00000000b        ; Granularity
+    db 0x00             ; Base (high 8 bits)
+
+    .end:
 
 align 4
 multiboot_header:
